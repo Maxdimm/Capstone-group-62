b8c24c11b3d77705aa098b3ccf4378ba81672b18
Courtney Bonn <courtbonn@gmail.com>
2017-01-31 13:02:45 -0800

    printed json data onto app

--- iOS/Calvary Chapel/Calvary Chapel/SecondViewController.swift
+++ iOS/Calvary Chapel/Calvary Chapel/SecondViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var jsonlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
     }
     
     @IBAction func JSONData(_ sender: UIButton) {
         
         restapisample()
+
+
     }
+ 
+
+    
     func restapisample() {
         // Set up URL request
         let todoEndpoint: String = "https://jsonplaceholder.typicode.com/todos/1"
@@ -32,49 +40,50 @@ class SecondViewController: UIViewController {
         
         let task = session.dataTask(with: urlRequest) {
             (data, response, error) in
-            //check for any errors
+            // check for any errors
             guard error == nil else {
                 print("error calling GET on /todos/1")
                 print(error!)
                 return
             }
-            //make sure we got data
+            // make sure we got data
             guard let responseData = data else {
                 print("Error: did not receive data")
                 return
             }
-            //parse the result as JSON
+            // parse the result as JSON, since that's what the API provides
             do {
                 guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
                     as? [String: Any] else {
                         print("error trying to convert data to JSON")
                         return
                 }
-                
-                //now we have the todo
-                //let's just print it to prove we can access it
+                // now we have the todo
+                // let's just print it to prove we can access it
                 print("The todo is: " + todo.description)
                 
-                //the todo object is a dictionary
-                //so we just access the title using the "title" key
-                //so check for a title and print it if we have one
+                // the todo object is a dictionary
+                // so we just access the title using the "title" key
+                // so check for a title and print it if we have one
                 guard let todoTitle = todo["title"] as? String else {
                     print("Could not get todo title from JSON")
                     return
                 }
+                //NEW CODE
+                DispatchQueue.main.async{
+                    self.jsonlabel.text = todoTitle
+                }
+                
                 print("The title is: " + todoTitle)
-            } catch {
+            } catch  {
                 print("error trying to convert data to JSON")
                 return
-                
             }
-            
         }
-        
-        
         task.resume()
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
