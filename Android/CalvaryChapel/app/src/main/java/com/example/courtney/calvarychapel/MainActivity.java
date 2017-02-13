580ba7c0b07dc515e0b7590792dbef2a66096302
Courtney Bonn <courtbonn@gmail.com>
2017-02-12 19:10:47 -0800

    added labels and logos to each page

--- Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/MainActivity.java
+++ Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.courtney.calvarychapel;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -101,4 +103,7 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
+
