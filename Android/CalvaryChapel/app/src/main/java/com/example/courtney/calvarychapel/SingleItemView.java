337fe9cc8aea1698d4a50209de6b814ed64a854b
Courtney Bonn <courtbonn@gmail.com>
2017-04-13 21:10:27 -0700

    added base files for XMLParser

---
+++ Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/SingleItemView.java
@@ -0,0 +1,23 @@
+package com.example.courtney.calvarychapel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Courtney on 4/13/17.
+ */
+
+public class SingleItemView extends Activity {
+    String eventname;
+    String eventdate;
+    String position;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.singleitemview);
+
+        //rest of code here
+    }
+}
---
+++ Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/XMLParser.java
@@ -0,0 +1,8 @@
+package com.example.courtney.calvarychapel;
+
+/**
+ * Created by Courtney on 4/13/17.
+ */
+
+public class XMLParser {
+}
