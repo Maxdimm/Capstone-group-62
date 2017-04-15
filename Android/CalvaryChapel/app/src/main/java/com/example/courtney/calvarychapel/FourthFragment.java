3bcd8f32838833a5d9dfbd580e9a0ec885d71a69
Courtney Bonn <courtbonn@gmail.com>
2017-04-14 20:41:09 -0700

    moved code from fourth fragment to the correct, second fragent

--- Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/FourthFragment.java
+++ Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/FourthFragment.java
@@ -1,30 +1,20 @@
 package com.example.courtney.calvarychapel;
 
 import android.app.Fragment;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by Courtney on 2/4/17.
- * Events Page
+ * Messages Page
  */
 
 public class FourthFragment extends Fragment {
 
     View myView;
-    ListView listView;
-    ListViewAdapter adapter;
-    ArrayList<HashMap<String,String>> arrayList;
-    static String NAME = "event_name";
-    static String DATE = "date";
 
     @Nullable
     @Override
@@ -36,22 +26,6 @@ public class FourthFragment extends Fragment {
         return myView;
     }
 
-    protected class DownloadXML extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            //code here
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void args) {
-            listView = (ListView) myView.findViewById(R.id.listview);
-            adapter = new ListViewAdapter(getActivity(), arrayList);
-            listView.setAdapter(adapter);
-        }
-
-    }
 }
 
 
--- Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/SecondFragment.java
+++ Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/SecondFragment.java
@@ -1,11 +1,16 @@
 package com.example.courtney.calvarychapel;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Courtney on 2/4/17.
@@ -13,6 +18,12 @@ import android.view.ViewGroup;
 
 public class SecondFragment extends Fragment {
 
+
+    ListView listView;
+    ListViewAdapter adapter;
+    ArrayList<HashMap<String,String>> arrayList;
+    static String NAME = "event_name";
+    static String DATE = "date";
     View myView;
 
     @Nullable
@@ -20,5 +31,22 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.second_layout, container, false);
         return myView;
+    }
+
+    protected class DownloadXML extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            //code here
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void args) {
+            listView = (ListView) myView.findViewById(R.id.listview);
+            adapter = new ListViewAdapter(getActivity(), arrayList);
+            listView.setAdapter(adapter);
+        }
+
     }
 }
