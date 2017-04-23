abef599dcb75ffe0663cdcc4265f7eadc822a3e4
Courtney Bonn <courtbonn@gmail.com>
2017-04-22 18:40:47 -0700

    working on XML Parser

--- Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/XMLParser.java
+++ Android/CalvaryChapel/app/src/main/java/com/example/courtney/calvarychapel/XMLParser.java
@@ -1,8 +1,120 @@
 package com.example.courtney.calvarychapel;
 
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Courtney on 4/13/17.
  */
 
 public class XMLParser {
+
+    private static final String ns = null;
+
+    public static class Entry {
+        public final String event_name;
+        public final String date;
+
+        private Entry(String event_name, String date) {
+            this.event_name = event_name;
+            this.date = date;
+        }
+    }
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "ccb_api");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("item")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "item");
+        String event_name = null;
+        String date = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if(name.equals("event_name")) {
+                event_name = readEventName(parser);
+            } else if (name.equals("date")) {
+                date = readDate(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Entry(event_name, date);
+    }
+
+    private String readEventName(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "event_name");
+        String event_name = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "event_name");
+        return event_name;
+    }
+
+    private String readDate(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "date");
+        String event_name = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "date");
+        return event_name;
+    }
+
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
 }
