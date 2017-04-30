15a22c2c5ef6fc18fb26daee54cb2aae6ac37aad
Max Dimm <dimmm@oregonstate.edu>
2017-04-30 14:43:18 -0700

    Fixed sermons page and redesigned button

--- iOS/Calvary Chapel/Calvary Chapel/FifthViewController.swift
+++ iOS/Calvary Chapel/Calvary Chapel/FifthViewController.swift
@@ -17,6 +17,7 @@ class FifthViewController: UIViewController {
         let youtubeURL = "https://livestream.com/accounts/18343788/events/6975672/videos/148653091/player?width=640&height=360&enableInfo=true&defaultDrawer=&autoPlay=true&mute=false"
         
         videoViewer.allowsInlineMediaPlayback = true
+        videoViewer.scalesPageToFit = true
         
         videoViewer.loadHTMLString("<iframe width=\(videoViewer.frame.width)\" height=\"\(videoViewer.frame.height)\" src=\"\(youtubeURL)?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)
     }
